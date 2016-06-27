e97008d9ba5438732c1c57daa2ff879ee0fb70b9
芝 朋美 <follwthedot@gmail.com>
2016-06-28 00:05:01 +0900

    Format Data, Build a Resume Header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,6 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedName = HTMLheaderName.replace("%data%","Tomomi Shiba");
+
+var role ="Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
