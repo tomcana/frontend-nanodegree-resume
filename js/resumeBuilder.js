1095856e15b3ae75c8d5bbad520e3d6da83a4030
芝 朋美 <follwthedot@gmail.com>
2016-11-21 07:19:58 +0900

    projectの表示、contactsの途中

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,7 +9,7 @@ var bio = {
 	"name":"Tomomi Shiba",
 	"role":"web developer",
 	"contacts":{
-		"email":"follwthedot@gmail.com",
+		"email":"XXX@gmail.com",
 		"mobile":"XXX-XXXX-XXXX",
 		"github":"tomcana",
 		"location":"Matsuyama city,Ehime Japan"
@@ -18,6 +18,21 @@ var bio = {
 	"welcomeMessage":"WELCOME!",
 	"skills":["awesomeness","programming","teaching","js"]
 };
+
+function displaycontact(){
+for(contact in bio.contacts){
+	var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
+	var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
+	var formattedTwitter = HTMLtwitter.replace("%data%",bio.contacts.twitter);
+	var formattedGithub = HTMLgithub.replace("%data%",bio.contacts.github);
+	var formattedBlog = HTMLblog.replace("%data%",bio.contacts.blog);
+	var formattedLocation = HTMLlocation.replace("%data%",bio.contacts.location);
+	$("#topContacts, #footerContacts").append(formattedMobile, formattedEmail, formattedGithub, formattedLocation); //joint jQuery feature to add contacts to header and footer
+
+}
+};
+
+displaycontact();
 
 var work = {
 	"jobs": [{
@@ -34,31 +49,21 @@ var work = {
 	]
 };
 
-var projects = {
-	"projects": [{
-		"title": "Portfolio Site",
-		"dates": "June 2016",
-		"description": "Single page, responsive website.",
-		"images": "img.jpg"
-	}, {
-		"title": "Styling spritz landing page",
-		"dates": "February 2015",
-		"description": "Landing page built to sell the styling spirits through affiliate links.",
-		"images": "img.jpg"
-	}]
-};
+
 
 var education = {
 	"schools": [{
 		"name": "Conestoga College",
 		"location": "Kitchener,ON,Canada",
 		"major": "General Business",
+		"degree": "Diploma",
 		"YearofGraducation": "2007",
 		"URL": "http://www.conestogac.on.ca/index.jsp"
 	}, {
 		"name": "Japan College of Foreigh Languages",
 		"location": "Tokyo,Japan",
-		"major": "Canada",
+		"major": "Canada Study",
+		"degree": "Diploma",
 		"YearofGraducation": "2004",
 		"URL": "http://www.jcfl.ac.jp/"
 	}],
@@ -74,6 +79,8 @@ var education = {
 var mybiopic = HTMLbioPic.replace("%data%",bio.biopic);
 $("#header").prepend(mybiopic);
 
+
+
 if(bio.skills.length > 0){
 	$("#header").append(HTMLskillsStart);
 	var formattedSkill = HTMLskills.replace("%data%",bio.skills[0]);
@@ -84,7 +91,7 @@ if(bio.skills.length > 0){
 	$("#skills").append(formattedSkill);
 	var formattedSkill = HTMLskills.replace("%data%",bio.skills[3]);
 	$("#skills").append(formattedSkill);
-};
+}
 
 function displaywork(){
 for(job in work.jobs){
@@ -101,9 +108,45 @@ for(job in work.jobs){
 	$(".work-entry:last").append(formattedDescription);
 	
 }
-};
+}
 
 displaywork();
+
+function displayeducation(){
+for(school in education.schools){
+	$("#education").append(HTMLschoolStart);
+	var formattedschoolName = HTMLschoolName.replace("%data%",education.schools[school].name);
+	var formattedschoolDegree = HTMLschoolDegree.replace("%data%",education.schools[school].degree);
+	var formattedschoolDates = HTMLschoolDates.replace("%data%",education.schools[school].YearofGraducation);
+	var formattedschoolLocation = HTMLschoolLocation.replace("%data%",education.schools[school].location);
+	var formattedschoolMajor = HTMLschoolMajor.replace("%data%",education.schools[school].major);
+	var formattedEducationTitle = formattedschoolName + formattedschoolDegree;
+	$(".education-entry:last").append(formattedEducationTitle);
+	$(".education-entry:last").append(formattedschoolDates);
+	$(".education-entry:last").append(formattedschoolLocation);
+	$(".education-entry:last").append(formattedschoolMajor);
+}
+}
+
+displayeducation();
+
+
+function displayonlineClasses(){
+for(onlineCourse in education.onlineCourses){
+	$("#education").append(HTMLschoolStart);
+	var formattedonlineTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses[onlineCourse].title);
+	var formattedonlineSchool = HTMLonlineSchool.replace("%data%",education.onlineCourses[onlineCourse].school);
+	var formattedonlineDates = HTMLonlineDates.replace("%data%",education.onlineCourses[onlineCourse].dates);
+	var formattedonlineURL = HTMLonlineURL.replace("%data%",education.onlineCourses[onlineCourse].URL);
+	var formattedonlineHead = formattedonlineTitle + formattedonlineSchool;
+	$(".education-entry:last").append(HTMLonlineClasses);
+	$(".education-entry:last").append(formattedonlineHead);
+	$(".education-entry:last").append(formattedonlineDates);
+	$(".education-entry:last").append(formattedonlineURL);
+}
+}
+
+displayonlineClasses();
 
 $(document).click(function(loc){
 	var x = loc.pageX;
@@ -144,12 +187,30 @@ function inName(name){
 
 $("#main").append(internationalizeButton);
 
+var projects = {
+	"projects": [{
+		"title": "Portfolio Site",
+		"url": "http://howtowedding.xsrv.jp/portfolio/",
+		"dates": "June 2016",
+		"description": "Single page, responsive website.",
+		"images": ["./images/portfolio_img01.jpg","./images/portfolio_img02.jpg"]
+
+	}, {
+		"title": "Styling spritz landing page",
+		"url": "http://howtowedding.xsrv.jp/haru/",
+		"dates": "February 2015",
+		"description": "Landing page built to sell the styling spirits through affiliate links.",
+		"images": ["./images/haru_img01.jpg","./images/haru_img02.jpg"]
+	}]
+};
+
 projects.display = function(){
 	for(project in projects.projects){
 	$("#projects").append(HTMLprojectStart);
 
 	var formattedTitle = HTMLprojectTitle.replace("%data%",projects.projects[project].title);
-	$(".project-entry:last").append(formattedTitle);
+	var linkedTitle = formattedTitle.replace("#",projects.projects[project].url);
+	$(".project-entry:last").append(linkedTitle);
 
 	var formattedDates = HTMLprojectDates.replace("%data%",projects.projects[project].dates);
 	$(".project-entry:last").append(formattedDates);
